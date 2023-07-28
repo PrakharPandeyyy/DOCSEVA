0d2b3ad07721265f7216b5cf392bc809715a2e76
prakharpandeyyy <8.prakharpandey@gmail.com>
2023-07-28 23:33:17 +0530

    added some ejs

--- app.js
+++ app.js
@@ -1,40 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const path = require('path')
+const path = require("path");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/views", express.static(__dirname + "/views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "views")));
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/public', express.static(__dirname + "/public"));
-
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-
-
-app.get("/",function(req,res){
-
-    res.render("index");
-
+app.get("/", function (req, res) {
+  res.render("index");
 });
-app.get("/blog",function(req,res){
-
+app.get("/blog", function (req, res) {
   res.render("blog");
-
 });
-app.get("/temp",function(req,res){
-  
-      res.render("template1");
-  
-})
+app.get("/loginPage", function (req, res) {
+  res.render("loginPage");
+});
+app.get("/temp", function (req, res) {
+  res.render("template1");
+});
 
-app.listen(3000,function(req,res){
-    console.log("Server is online on Port:3000");
+app.listen(3000, function (req, res) {
+  console.log("Server is online on Port:3000");
 });
